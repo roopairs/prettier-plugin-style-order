4c2644dc90a0dde7ea54cbe539b7e691bfded79c
Abraão Azevedo <az.abraao@gmail.com>
2022-12-17 17:36:05 -0300

    fix sorting

--- src/config/sorter.js
+++ src/config/sorter.js
@@ -1,9 +1,13 @@
 const postcss = require('postcss')
-const sorting = require('postcss-sorting');
-const createConfig = require('./factory');
+const sorting = require('postcss-sorting')
+const createConfig = require('./factory')
 
 module.exports = (value, opts = {}) => {
-    return postcss({
-        plugins: [sorting(createConfig())]
-    }).process(value, { from: undefined }).css;
+  return postcss({
+    plugins: [
+      sorting({
+        'properties-order': createConfig(),
+      }),
+    ],
+  }).process(value, { from: undefined }).css
 }
